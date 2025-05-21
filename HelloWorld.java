c9544abc3f3ccc31e45e67bf73c97fde4b916b2a
Jason Costa <jzcosta180@student.cccd.edu>
2025-05-21 10:39:03 -0700

    New HelloWorld.java file

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+
+  // This class defines the output for HelloWorld
+public class HelloWorld{
+public static void main(String[] args){
+  System.out.println("Hello world, my name is Andru Morales");
+ }
+}
